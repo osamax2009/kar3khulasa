e983b289f5cc2f80358e6daa66a90b3e2d855b5b
osamax2009 <osamafalah85@yahoo.com>
2024-07-17 07:35:10 +0300

    Initial Commit

---
+++ kar3khulasa/kar3khulasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  kar3khulasaApp.swift
+//  kar3khulasa
+//
+//  Created by Osama shallal on 17/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct kar3khulasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
